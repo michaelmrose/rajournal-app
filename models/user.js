3585b35a88e10c1e324425c985f5e50a0b22a772
Michael Rose <michael@rosenetwork.net>
2023-09-08 17:01:04 -0700

    basic models

--- models/user.js
+++ models/user.js
@@ -4,6 +4,61 @@ const bcrypt = require('bcrypt');
 
 const SALT_ROUNDS = 6;
 
+const journalEntrySchema = new Schema({
+    creationDate: {
+      type: Date,
+      required: true,
+      default: Date.now()
+    },
+    painLevel: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 10,
+    },
+    stiffnessLevel: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 10,
+    },
+    fatigueLevel: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 10,
+    },
+})
+
+const lifeEventSchema = new Schema ({
+    date: {
+      type: Date,
+      required: true,
+      default: Date.now()
+    },
+    event: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 300,
+
+    } })
+
+const noteSchema = new Schema({
+    creationDate: {
+      type: Date,
+      required: true,
+      default: Date.now()
+    },
+  contents: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 1000,
+  }})
+
+
+
 const userSchema = new Schema({
   name: {type: String, required: true},
   email: {
@@ -16,7 +71,12 @@ const userSchema = new Schema({
   password: {
     type: String,
     required: true
-  }
+  },
+notes: [noteSchema],
+lifeEvents: [lifeEventSchema],
+journalEntries: [journalEntrySchema]
+
+
 }, {
   timestamps: true,
   toJSON: {
