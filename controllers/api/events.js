604a67b15005074cfe9ff9347acf901ed6ccb6b9
Michael Rose <michael@rosenetwork.net>
2023-09-10 21:50:25 -0700

    comments

--- controllers/api/events.js
+++ controllers/api/events.js
@@ -7,13 +7,17 @@ module.exports = {
 
 }
 
+// get details of a specific event
 function detail(req,res){
 
 }
+
+//update details of a specific event
 function update(req,res){
 
 }
 
+//delete a particular event
 function deleteEvent(req,res){
 
 }
