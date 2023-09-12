80629035d9dadf1cccad604b84ba9c9e8fc90a54
Michael Rose <michael@rosenetwork.net>
2023-09-12 01:23:02 -0700

    theme belongs at the root

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Theme, ThemePanel, Box } from '@radix-ui/themes';
 import './index.css';
 import App from './pages/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router><App /></Router>
+    <Router>
+    <Theme  appearance='light' hasBackground="true"  accentColor='violet' radius="large" scaling='95%'>
+        <App /> 
+        </Theme>
+        </Router>
   </React.StrictMode>
 );
 
